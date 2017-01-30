989443a3e83ff31035fab1c5f928c4597d474424
Irakli Safareli <i.safareli@gmail.com>
2017-01-31 01:03:37 +0400

    reorder/rename internal functions

--- src/daggy.js
+++ src/daggy.js
@@ -1,15 +1,109 @@
 const { toString } = require('sanctuary-type-classes')
 const type = require('sanctuary-type-identifiers')
 
-// Name of prop used to store:
+// Names of prop used to store:
 // * name of variant of a sum type
 const TAG = '@@tag'
 // * array of arguments used to create a value (to speed up `cata`)
 const VALUES = '@@values'
 // * `@@type` of it's returned results
 const TYPE = '@@type'
-// * `TYPE` of it's returned results
+// * `@@type` of variant constructor's returned results
 const RET_TYPE = '@@ret_type'
+
+const tagged = (typeName, fields) => {
+  const proto = {}
+  proto.toString = tagged$toString
+  // this way we avoid named function
+  const typeRep = (0, (...args) => makeValue(fields, proto, args))
+  typeRep.toString = typeRepToString
+  typeRep.prototype = proto
+  typeRep.is = isType
+  typeRep[TYPE] = typeName
+  proto.constructor = typeRep
+  return typeRep
+}
+
+const taggedSum = (typeName, constructors) => {
+  const proto = {}
+  proto.cata = sum$cata
+  proto.toString = sum$toString
+  const typeRep = {
+    toString: typeRepToString,
+    prototype: proto,
+    is: isType,
+    [TYPE]: typeName
+  }
+  proto.constructor = typeRep
+  Object.keys(constructors).forEach(tag => {
+    const fields = constructors[tag]
+    const tagProto = Object.create(proto)
+    defProp(tagProto, TAG, tag)
+    if (fields.length === 0) {
+      typeRep[tag] = makeValue(fields, tagProto, [])
+      typeRep[tag].is = sum$isUnit
+      return
+    }
+    typeRep[tag] = (...args) => makeValue(fields, tagProto, args)
+    typeRep[tag].is = sum$isVariant
+    typeRep[tag][TAG] = tag
+    typeRep[tag][RET_TYPE] = typeName
+    typeRep[tag].toString = sum$ctrToString
+  })
+  return typeRep
+}
+
+const sum$cata = function (fs) {
+  const tag = this[TAG]
+  if (!fs[tag]) {
+    throw new TypeError("Constructors given to cata didn't include: " + tag)
+  }
+  return fs[tag].apply(fs, this[VALUES])
+}
+
+const sum$ctrToString = function () {
+  return `${this[RET_TYPE]}.${this[TAG]}`
+}
+
+const sum$toString = function () {
+  return `${this.constructor[TYPE]}.${this[TAG]}${arrToString(this[VALUES])}`
+}
+
+const typeRepToString = function () {
+  return this[TYPE]
+}
+
+const tagged$toString = function () {
+  return `${this.constructor[TYPE]}${arrToString(this[VALUES])}`
+}
+
+const sum$isVariant = function (val) {
+  return Boolean(val) &&
+    this[TAG] === val[TAG] &&
+    this[RET_TYPE] === type(val)
+}
+
+const sum$isUnit = function (val) {
+  return this === val || Boolean(val) &&
+    this[TAG] === val[TAG] &&
+    type(this) === type(val)
+}
+
+const isType = function (val) {
+  return this[TYPE] === type(val)
+}
+
+const makeValue = (fields, proto, values) => {
+  if (values.length !== fields.length) {
+    throw new TypeError(`Expected ${fields.length} arguments, got ${values.length}`)
+  }
+  const obj = Object.create(proto)
+  defProp(obj, VALUES, values)
+  for (let idx = 0; idx < fields.length; idx++) {
+    obj[fields[idx]] = values[idx]
+  }
+  return obj
+}
 
 // adopted version of withValue from  https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
 const defProp = (obj, prop, val) => {
@@ -35,100 +129,6 @@ const arrToString = (arr) => {
     str += ', ' + toString(arr[i])
   }
   return str + ')'
-}
-
-const tagged = (typeName, fields) => {
-  const proto = {}
-  proto.toString = tagged$objToString
-  // this way we avoid named function
-  const typeRep = (0, (...args) => makeValue(fields, proto, args))
-  typeRep.toString = typeRepToString
-  typeRep.prototype = proto
-  typeRep.is = isType
-  typeRep[TYPE] = typeName
-  proto.constructor = typeRep
-  return typeRep
-}
-
-const taggedSum = (typeName, definitions) => {
-  const proto = {}
-  proto.cata = taggedSum$cata
-  proto.toString = taggedSum$objToString
-  const typeRep = {
-    toString: typeRepToString,
-    prototype: proto,
-    is: isType, // [1]
-    [TYPE]: typeName
-  }
-  proto.constructor = typeRep
-  Object.keys(definitions).forEach(tag => {
-    const fields = definitions[tag]
-    const tagProto = Object.create(typeRep.prototype)
-    defProp(tagProto, TAG, tag)
-    if (fields.length === 0) {
-      typeRep[tag] = makeValue(fields, tagProto, [])
-      typeRep[tag].is = taggedSum$isUnitValue
-      return
-    }
-    typeRep[tag] = (...args) => makeValue(fields, tagProto, args)
-    typeRep[tag].is = taggedSum$isVariant
-    typeRep[tag][TAG] = tag
-    typeRep[tag][RET_TYPE] = typeName
-    typeRep[tag].toString = taggedSum$constructorToString
-  })
-  return typeRep
-}
-
-const taggedSum$cata = function (fs) {
-  const tag = this[TAG]
-  if (!fs[tag]) {
-    throw new TypeError("Constructors given to cata didn't include: " + tag)
-  }
-  return fs[tag].apply(fs, this[VALUES])
-}
-
-const taggedSum$constructorToString = function () {
-  return `${this[RET_TYPE]}.${this[TAG]}`
-}
-
-const typeRepToString = function () {
-  return this[TYPE]
-}
-
-const taggedSum$objToString = function () {
-  return `${this.constructor[TYPE]}.${this[TAG]}${arrToString(this[VALUES])}`
-}
-
-const taggedSum$isVariant = function (val) {
-  return Boolean(val) &&
-    this[TAG] === val[TAG] &&
-    this[RET_TYPE] === type(val)
-}
-
-const taggedSum$isUnitValue = function (val) {
-  return this === val || Boolean(val) &&
-    this[TAG] === val[TAG] &&
-    type(this) === type(val)
-}
-
-const isType = function (val) {
-  return this[TYPE] === type(val)
-}
-
-const tagged$objToString = function () {
-  return `${this.constructor[TYPE]}${arrToString(this[VALUES])}`
-}
-
-const makeValue = (fields, proto, values) => {
-  if (values.length !== fields.length) {
-    throw new TypeError(`Expected ${fields.length} arguments, got ${values.length}`)
-  }
-  const obj = Object.create(proto)
-  defProp(obj, VALUES, values)
-  for (let idx = 0; idx < fields.length; idx++) {
-    obj[fields[idx]] = values[idx]
-  }
-  return obj
 }
 
 module.exports = {
