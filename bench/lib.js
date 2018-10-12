29a22bad222c1e4dda67b8178c471a3ba57a9b92
Seggy Umboh <seg@seg.gy>
2018-10-12 01:27:09 -0700

    Check all cases on cata

--- bench/lib.js
+++ bench/lib.js
@@ -9,26 +9,56 @@ const ListDef = {
   Cons: ['x', 'xs'],
   Nil: []
 }
+const GiantDef = {
+  V0: [],
+  V1: ['a'],
+  V2: ['a', 'b'],
+  V3: ['a', 'b', 'c'],
+  V4: ['a', 'b', 'c', 'd'],
+  V5: ['a', 'b', 'c', 'd', 'e'],
+  V6: ['a', 'b', 'c', 'd', 'e', 'f'],
+  V7: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+  V8: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+  V9: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+}
 
 const TupleOld = daggyOld.tagged('Tuple', TupleDef)
 const ListOld = daggyOld.taggedSum('List', ListDef)
+const GiantOld = daggyOld.taggedSum('Giant', GiantDef)
 const listOld = ListOld.Cons(1, ListOld.Nil)
 const tupleOld = TupleOld(1, 2)
+const giantOld = GiantOld.V1(1)
 
 const TupleNew = daggyNew.tagged('Tuple', TupleDef)
 const ListNew = daggyNew.taggedSum('List', ListDef)
+const GiantNew = daggyNew.taggedSum('Giant', GiantDef)
 const listNew = ListNew.Cons(1, ListNew.Nil)
 const tupleNew = TupleNew(1, 2)
+const giantNew = GiantNew.V1(1)
 
 const pattern = {
   Cons: (x, xs) => 1,
   Nil: () => 0
 }
 
+const giantPattern = {
+  V0: () => 0,
+  V1: () => 1,
+  V2: () => 2,
+  V3: () => 3,
+  V4: () => 4,
+  V5: () => 5,
+  V6: () => 6,
+  V7: () => 7,
+  V8: () => 8,
+  V9: () => 9
+}
+
 const defEnv = {
   ListDef,
   TupleDef,
-  pattern
+  pattern,
+  giantPattern
 }
 const env = {
   Old: Object.assign({}, defEnv, {
@@ -37,7 +67,8 @@ const env = {
     List: ListOld,
     list: listOld,
     taggedSum: daggyOld.taggedSum,
-    tagged: daggyOld.tagged
+    tagged: daggyOld.tagged,
+    giant: giantOld
   }),
   New: Object.assign({}, defEnv, {
     tuple: tupleNew,
@@ -45,7 +76,8 @@ const env = {
     List: ListNew,
     list: listNew,
     taggedSum: daggyNew.taggedSum,
-    tagged: daggyNew.tagged
+    tagged: daggyNew.tagged,
+    giant: giantNew
   })
 }
 
